600bfd35cfdcbf2a74de582a6384664139aed684
Subrata Panja <subrata@fortunaindia.com>
2019-05-10 11:10:34 +0530

    initial commit

--- WebApiWithSwagger/Startup.cs
+++ WebApiWithSwagger/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebApiWithSwagger
 {
@@ -26,6 +27,39 @@ namespace WebApiWithSwagger
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors(options =>
+            {
+                options.AddPolicy("all",
+                builder =>
+                {
+                    builder.WithOrigins()
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "My API",
+                    Version = "v1",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Subrata Panja",
+                        Email = "sbtpanja@gmail.com",
+                        Url = "https://getmybazaar.com"
+                    },
+                    License = new License
+                    {
+                        Name = "Use under MIT",
+                        Url = "https://getmybazaar.com/license"
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +76,17 @@ namespace WebApiWithSwagger
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors("all");
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseMvc();
         }
     }
